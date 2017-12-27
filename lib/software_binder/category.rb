a65ae8b83e1b3b4dd22e04d12e3ae665dc4927fb
Hoon Lee <yl.hoony@gmail.com>
2017-12-27 12:50:34 -0800

    added function to scrape categories

--- lib/software_binder/category.rb
+++ lib/software_binder/category.rb
@@ -1,3 +1,14 @@
 class SoftwareBinder::Category
+  attr_accessor :name, :slug
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- lib/software_binder/scraper.rb
+++ lib/software_binder/scraper.rb
@@ -3,7 +3,15 @@ class SoftwareBinder::Scraper
   def self.scrape_categories
     html = open("https://www.capterra.com/categories")
     doc = Nokogiri::HTML(html)
-    binding.pry
+    doc.css(".browse-group-list li a").each do |element|
+      category = SoftwareBinder::Category.new
+      category.name = element.text
+      category.slug = element.attribute("href").value
+    end
+  end
+
+  def self.scrape_softwares
+
   end
 
 end
