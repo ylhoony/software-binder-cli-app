6895759c6481d72c14a9577638fbec846ef8b911
Hoon Lee <yl.hoony@gmail.com>
2017-12-28 10:20:36 -0800

    removed unnecessary lines

--- lib/software_binder.rb
+++ lib/software_binder.rb
@@ -7,8 +7,3 @@ require "software_binder/cli"
 require "software_binder/scraper"
 require "software_binder/category"
 require "software_binder/software"
-
-
-# module SoftwareBinder
-  # Your code goes here...
-# end
