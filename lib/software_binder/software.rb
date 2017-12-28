a61468f6e733add50423097bb202d1562667cd9f
Hoon Lee <yl.hoony@gmail.com>
2017-12-27 23:43:25 -0800

    set up initialize method, instance variables

--- lib/software_binder/software.rb
+++ lib/software_binder/software.rb
@@ -1,5 +1,20 @@
 class SoftwareBinder::Software
+  attr_accessor :name, :overall_rating, :reviews, :description, :page_slug
+  attr_reader :category
 
+  @@all = []
 
+  def initialize(category)
+    @category = category
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
